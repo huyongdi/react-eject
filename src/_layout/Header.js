7452a3b6e6de15e3d259da92e464a0533fcdc2ac
354224080@qq.com <huyongdi525>
2019-01-18 15:36:09 +0800

    测试react-router替换reach-router：解决打包到线上，子页面刷新404的问题

--- src/_layout/Header.js
+++ src/_layout/Header.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react';
-import { Router, Link } from "@reach/router";
+//import { Router, Link } from "@reach/router";
+import {HashRouter as Router, Route, Link} from "react-router-dom";
 
 export default class Header extends Component {
   render() {
     return (
-        <div className='headerContent'>
-          <Link to="/">主页</Link>
-          <Link to="about">关于</Link>
-          <Link to="article">文章</Link>
-          <a href="oldVersion/index.html" target='_blank'>旧版</a>
-        </div>
+        <Router>
+          <div className='headerContent'>
+            <Link to="/">主页</Link>
+            <Link to="/about">关于</Link>
+            <Link to="/article">文章</Link>
+            <a href="oldVersion/index.html" target='_blank'>旧版</a>
+          </div>
+        </Router>
     );
   }
 }
--- src/_layout/Middle.js
+++ src/_layout/Middle.js
@@ -1,18 +1,26 @@
 import React, {Component} from 'react';
-import { Router, Link } from "@reach/router";
+//import { Router, Link } from "@reach/router";
+import {HashRouter as Router, Route, Link} from "react-router-dom";
 import Home from '../_pages/Home'
 import Article from '../_pages/Article'
 import About from '../_pages/About'
+
 export default class Header extends Component {
   render() {
     return (
-        <div className='middleContent'>
-          <Router>
-            <Home path="/" />
-            <About path="about" />
-            <Article path="article" />
-          </Router>
-        </div>
+        <Router>
+          <div className='middleContent'>
+            <Route path="/" exact component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/article" component={Article}/>
+
+            {/*<Router>*/}
+            {/*<Home path="/" />*/}
+            {/*<About path="about" />*/}
+            {/*<Article path="article" />*/}
+            {/*</Router>*/}
+          </div>
+        </Router>
     );
   }
 }
