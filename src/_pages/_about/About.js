d16eecae7d16743b6ee6c12bcb3ae04a0385f804
HYD <354224080@qq.com>
2019-12-23 14:59:47 +0800

    '关于初步修改一版'

--- src/_pages/_about/About.js
+++ src/_pages/_about/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Drawer, List, Avatar } from 'antd';
+import { Drawer, List, Avatar, Timeline } from 'antd';
 import styles from './index.module.less'
 import qq from '@img/qq.jpg'
 import Map from './map.js'
@@ -7,19 +7,28 @@ import Map from './map.js'
 export default class About extends Component {
   state = {
     visible: false, // 隐藏抽屉是否显示
-    drawerData: '', // 隐藏抽屉的内容
+    drawerData: [], // 隐藏抽屉的内容
     dataSource: [
       {
         id: 1,
         title: '程序猿',
         description: '努力奋斗，为实现中华民族的伟大复兴贡献自己的一份力',
-        data: '1234556'
+        data: [
+          '2011-2015 本科四年',
+          '2015-至今 毕业后一直从事前端工作' 
+        ]
       },
       {
         id: 2,
         title: 'DOTA2云玩家',
         description: '指点江山，口若悬河：首先我很久没打了，但是我看直播挺简单的，你打不过就是你太菜了',
-        data: '88888888'
+        data: [
+          '建筑',
+          '澄海3C',
+          'DOTA1',
+          'DOTA2',
+          '云玩家'
+        ]
       }
     ]
   };
@@ -50,7 +59,7 @@ export default class About extends Component {
             <List.Item
               key={item.id}
               actions={[
-                <span className={styles.detailBtn} onClick={()=>this.showDrawer(item.data)} key={`a-${item.id}`}>
+                <span className={styles.detailBtn} onClick={() => this.showDrawer(item.data)} key={`a-${item.id}`}>
                   详细
                 </span>
               ]}
@@ -72,7 +81,11 @@ export default class About extends Component {
           onClose={this.onClose}
           visible={visible}
         >
-          {drawerData}
+          <Timeline>
+            {drawerData.map((item,index) => {
+              return <Timeline.Item key={index}>{item}</Timeline.Item>
+            })}
+          </Timeline>
         </Drawer>
 
         {/* 在线百度地图 */}
--- src/_pages/_about/map.js
+++ src/_pages/_about/map.js
@@ -7,10 +7,15 @@ export default class Map extends Component {
     };
 
     componentDidMount() {
+        this.initMap()
+        
+    }
 
+    initMap() {
         var map = new BMap.Map("container");          // 创建地图实例  
-        var point = new BMap.Point(116.404, 39.915);  // 创建点坐标  
-        map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别  
+        var point = new BMap.Point(114.405733, 30.511933);  // 创建点坐标  
+        map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别 
+        map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
     }
 
     render() {
