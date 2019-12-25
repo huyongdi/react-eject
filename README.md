cd7ec4d73b0c9547ba4adef0dab2cbd401e01ee3
HYD <354224080@qq.com>
2019-12-25 15:07:36 +0800

    '初步使用微软自带预览'

--- README.md
+++ README.md
@@ -1,3 +1,77 @@
-#### 目的：用来测试和学习一些新东西
+<p align="center">
+<a href="javascript:viod(0)">
+<img width="200" src="http://huyongdi.com/favicon.ico">
+</a>
+</p>
 
-#### 技术选型：react+react-router+rematch+antd
+<h1 align="center">hyd-ui</h1>
+
+<h4 align="center">react按需加载组件库脚手架</h4>
+
+## 1: 安装 📦
+> 使用 npm
+```
+npm i hyd-ui --save
+```
+
+> 使用 yarn
+```
+yarn add hyd-ui
+```
+
+## 2: 使用(按需导入) 🔨
+> 1. 手动引入
+```js
+import Button from 'hyd-ui/lib/button'; // 加载 JS
+import 'hyd-ui/lib/button/style/css'; // 加载 CSS
+```
+> 2. 利用插件 [babel-plugin-import](https://github.com/ant-design/babel-plugin-import
+```js
+import { Button , Button1 } from "hyd-ui"
+
+// .babelrc.js 或加入到webpack.config.js的babel-loader中
+module.exports = {
+plugins: [
+["import", {
+"libraryName": "hyd-ui",
+"libraryDirectory": "es",
+"style": true
+}]
+]
+}
+
+// 项目引入了多个库
+module.exports = {
+plugins: [
+["import", {
+"libraryName": "antd",
+"libraryDirectory": "es",
+"style": true
+}],
+["import", {
+"libraryName": "hyd-ui",
+"libraryDirectory": "es",
+"style": true //需要项目已经支持less
+},'hyd-ui'],
+]
+}
+```
+## 3: 使用文档 🌍
+```js
+doc目录下运行index.html (通过storybook生成，待完善)
+目前有2个测试按钮(字体颜色不同)：
+class Test extends React.Component {
+render(){
+return (
+<Button text="毛衣"></Button>
+<Button1 text="花欠"></Button1>
+)
+}
+}
+```
+
+## 4: 参考文档 🔗
+- [ant-design](https://github.com/ant-design/ant-design)
+- [gulp](https://www.gulpjs.com.cn/docs/)
+- [webpack](https://www.webpackjs.com/)
+- [storybook](https://github.com/storybookjs/storybook)
--- src/_pages/_articles/newArticle.js
+++ src/_pages/_articles/newArticle.js
@@ -1,35 +1,17 @@
 import React, { Component } from 'react';
-import { Card, Drawer, Spin } from 'antd';
+import { Card, Drawer } from 'antd';
 import style from './article.module.less'
 import './article.less'
-import axios from 'axios'
-
-var mammoth = require("mammoth");
 // const { Meta } = Card;
-var options = {
-    styleMap: [
-        // "p => span.a",
-        // "r => span.b",
-        "i => p.code",
-        "u => p.codeInline",
-    
-        // "p[style-name='Subsection Title'] => h2:fresh"
-    ]
-};
 export default class NewArticle extends Component {
-
     state = {
-        visible: false, // 是否显示draw
-        loading: false, // 是否显示spin
         articleArr: [ //文档数组 和public/doc 对应
             {
                 id: 1,
                 title: '从零搭建类似antd的组件化项目',
-                description: '123'
+                description: 'https://github.com/huyongdi/hyd-ui'
             }
         ],
-        docDetail: '', // 当前文档内容
-        currentDoc: '' //当前显示的文档
     }
 
     componentDidMount() {
@@ -38,33 +20,11 @@ export default class NewArticle extends Component {
 
     // 查看文章详情
     showDetail = (item) => () => {
-        const that = this
-        this.setState({
-            visible: true,
-            loading: true,
-            currentDoc: item
-        }, async () => {
-            let respDoc = await axios.get(`${process.env.PUBLIC_URL}/doc/${item.id}.docx`, { responseType: 'arraybuffer' })
-                mammoth.convertToHtml({ arrayBuffer: respDoc.data }, options)
-                .then(function (result) {
-                    that.setState({
-                        loading: false,
-                        docDetail: result.value
-                    })
-                    // var html = result.value; // The generated HTML
-                    // var messages = result.messages; // Any messages, such as warnings during conversion
-                })
-        });
+        window.open(encodeURI(`https://view.officeapps.live.com/op/view.aspx?src=http://www.huyongdi.com/doc/${item.title}.docx?${Math.random()}`))
     }
 
-    onClose = () => {
-        this.setState({
-            visible: false,
-        });
-    };
-
     render() {
-        const { visible, docDetail, loading, articleArr, currentDoc } = this.state
+        const { articleArr } = this.state
         return (
             <div className={style.articleNew}>
                 <div className={style.cardContent}>
@@ -79,29 +39,12 @@ export default class NewArticle extends Component {
                                 {/* <Meta title={item.title} description={item.description} /> */}
                                 <div className="ant-card-meta-detail">
                                     <div className="ant-card-meta-title" title={item.title}>{item.title}</div>
-                                    <div className="ant-card-meta-description" title={item.description}>{item.description}</div>
+                                    <a   className="ant-card-meta-description" href={item.description} title={item.description}>{item.description}</a>
                                 </div>
                             </Card>
                         })
                     }
-
                 </div>
-
-                {/* 文章详情抽屉 */}
-                <Drawer
-                    title={currentDoc.title}
-                    placement="right"
-                    closable={false}
-                    onClose={this.onClose}
-                    visible={visible}
-                    width='50vw'
-                    bodyStyle={{
-                        width: '50vw'
-                    }}
-                >
-                    <Spin className={style.spin} size="large" spinning={loading}></Spin>
-                    <div className={style.docDetailContent} dangerouslySetInnerHTML={{ __html: docDetail }}></div>
-                </Drawer>
             </div>
         );
     }
